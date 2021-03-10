882429ba518d24352ecc437d6f87bf45db821032
Lara Komosa <lara.komosa@lt036.attlocal.net>
2021-03-10 11:07:10 -0600

    2nd Commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 34.1;
+            double y = 23.4;
+            var z = x + y;
+            Console.WriteLine(z);
+            if(args.Length>0)
+            {
+            Console.WriteLine("Hello" + args[0] + "!");
+        }
+        else
+        {
+            Console.WriteLine("Yello!");
+        }
+        }
+        }}
