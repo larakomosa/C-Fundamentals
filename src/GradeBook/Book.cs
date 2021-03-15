f8e4d76a6eb71263d1b128b7ac99632aba89c44c
Lara Komosa <lara.komosa@lt036.attlocal.net>
2021-03-15 11:25:36 -0500

    switches

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Runtime.Intrinsics.X86;
 using System.Globalization;
 using System.Collections.Generic;
 
@@ -5,11 +7,54 @@ namespace GradeBook
 {
     class Book
     {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            Name = name;
+        }
+
         public void AddGrade(double grade)
         {
-
+            grades.Add(grade);
         }
-        List<double> grades;
-    }
 
-}
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics()
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            foreach (var grade is grades)
+            {
+                result.Low = Math.Min(grade, result.Low);
+                result.High = Math.Max(grade, result.High);
+                result.Average += grade;
+            }
+            result.Average /= grades.Count;
+
+            switch (result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+
+
+            }
+            List<double> grades;
+        }
+
+    }
